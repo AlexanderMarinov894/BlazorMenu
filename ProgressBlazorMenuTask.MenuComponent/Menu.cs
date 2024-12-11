7b5dfa954a7308c3d85e3be71cd21a10ecc9c471
alexa <alexa@ALEXANDER-PC>
2024-12-11 17:01:12 +0200

    Add project files.

---
+++ ProgressBlazorMenuTask.MenuComponent/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressBlazorMenuTask.MenuComponent
+{
+    public class MenuModel 
+    {
+        public List<MenuItemModel>? Items { get; set; }
+    }
+    public class MenuItemModel
+    {
+        public required string Text { get; set; }
+        public List<MenuItemModel>? Items { get; set; }
+    }
+
+    public enum MenuOpenType 
+    {
+        MouseEnter,
+        Click
+    }
+}
